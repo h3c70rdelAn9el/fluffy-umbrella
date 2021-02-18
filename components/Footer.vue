7696d826a5f3995eadefd862f5db46ca3e00302d
Hector del Angel <hectordelangel@gmail.com>
2021-02-18 11:52:05 -0800

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="footer" class="footer">
+    <b-row>
+      <b-col>
+        <p>
+          pictures from <a href="https://placebeard.it/">placebeard.it</a> &
+          <a href="www.picsum.com">picsum</a>
+        </p>
+      </b-col>
+      <b-col class="footer-right">
+        <p>simple landing page using bootstrap-vue by h3c70r</p>
+        <p />
+        <p />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<style>
+.footer {
+  background-color: black;
+  color: rgb(213, 135, 233);
+  min-height: 100%;
+  padding: 10px;
+}
+
+.footer-right {
+  line-height: -11px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
     <!-- <Navbar /> -->
     <Navbar2 />
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
